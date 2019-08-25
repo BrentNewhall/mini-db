4c3a1a6a6841827c4d4657f45196603c1a159fe3
Brent P. Newhall <brent@brentnewhall.com>
2019-08-25 11:54:25 -0400

    Fix tag selection

--- src/Tag.js
+++ src/Tag.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import * as AWS from 'aws-sdk';
 import './App.css';
 
+import { getItemHTML } from './global';
+
 class Tag extends Component {
   constructor( props ) {
     super( props );
@@ -33,15 +35,8 @@ class Tag extends Component {
 
   render() {
     const items = this.state.items.map( (item, index) => {
-      if( item.tags.values.includes("mini") ) {
-        return <div className="row" key={index}>
-            <div className="col m2"><img src={item.preview_image_url} alt="thumbnail" className="preview-image" /></div>
-            <div className="col m20">
-                <h2><a href={item.link}>{item.name}</a></h2>
-                by <a href={item.author_email}>{item.author_name}</a><br />
-                [{item.tags !== "undefined" ? item.tags.values.map( (tag) => <span key={tag}><a href={"/tag?t=" + tag}>{tag}</a>,</span> ) : null}]
-            </div>
-            </div>
+      if( item.tags.values.includes(this.tag) ) {
+        return getItemHTML( item, index );
       }
       else {
         return null;
