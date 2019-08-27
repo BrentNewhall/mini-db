20fe32e1cd67111a9510960a73e8155ca2498d20
Brent P. Newhall <brent@brentnewhall.com>
2019-08-26 21:16:25 -0400

    Add form to Add Item page

--- src/AddItem.js
+++ src/AddItem.js
@@ -29,7 +29,31 @@ class AddItem extends Component {
           Mini DB - Add Item
         </header>
         <div className="container">
-          Add
+          <mat-form-field>
+            <mat-label>Item Name:</mat-label>
+            <input name="name" placeholder="Goblin" />
+          </mat-form-field>
+          <mat-form-field>
+            <mat-label>Author Name:</mat-label>
+            <input name="author_name" placeholder="John Q. Public" />
+          </mat-form-field>
+          <mat-form-field>
+            <mat-label>Author Email:</mat-label>
+            <input name="author_email" placeholder="john@public.com" />
+          </mat-form-field>
+          <mat-form-field>
+            <mat-label>Link:</mat-label>
+            <input name="link" placeholder="https://website.com/goblin" />
+          </mat-form-field>
+          <mat-form-field>
+            <mat-label>Preview Image URL:</mat-label>
+            <input name="link" placeholder="https://website.com/images/goblin.jpg" />
+          </mat-form-field>
+          <mat-form-field>
+            <mat-label>Tags:</mat-label>
+            <input name="tags" placeholder="mini,monster" />
+          </mat-form-field>
+          <button className="btn btn-primary">Add</button>
         </div>
       </div>
     );
